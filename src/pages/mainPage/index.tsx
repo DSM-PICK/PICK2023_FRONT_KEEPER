b3b58aaedcf73dcb93657246145ae299a1f90e41
inung1004 <dksdbswl10430@naver.com>
2023-06-25 23:19:18 +0900

    ⚡️ :: 외출자 목록 페이지 퍼블리싱

--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,5 +1,12 @@
+import { useQuery } from "react-query";
+import { useState } from "react";
+import { getOutingStudentList } from "../../utils/apis/outing";
+import OutList from "../../components/outing";
+
 const MainPage = () => {
-  return <></>;
+  const { data: outing } = useQuery(["outing"], () => getOutingStudentList());
+
+  return <OutList outing={outing?.data.outing || []} />;
 };
 
 export default MainPage;
